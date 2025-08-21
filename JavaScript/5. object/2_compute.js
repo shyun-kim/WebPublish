74f226fd9ed4369ce406b0e3049faaab1b0c6069
shyun <exriik@gmail.com>
2025-08-21 12:44:52 +0900

    250821

--- JavaScript/5. object/2_compute.js
+++ JavaScript/5. object/2_compute.js
@@ -1,3 +1,42 @@
-/**
- * 
- */
+import { setObject, getObject, updateObject, deleteObject } from '../commons/util.js'
+
+
+const person = {
+    name: '홍길동',
+    age: 20,
+    job: '개발자'
+}
+
+const fruits = {
+    name: 'apple',
+    emoji: '🍎'
+}
+
+
+
+setObject(person, 'address', '강남구');
+console.log(person);
+
+let job = getObject(person, 'job');
+console.log(job);
+
+updateObject(person, 'age', 32);
+console.log(person);
+
+deleteObject(person, 'job')
+console.log(person);
+
+console.log('==================');
+
+
+setObject(fruits, 'color', 'red');
+console.log(fruits);
+
+let e = getObject(fruits, 'emoji');
+console.log(e);
+
+updateObject(fruits, 'color', 'green');
+console.log(fruits);
+
+deleteObject(fruits, 'emoji')
+console.log(fruits);
--- JavaScript/commons/util.js
+++ JavaScript/commons/util.js
@@ -5,3 +5,30 @@ export function toNumber(a,b) {
         b: parseInt(b)
     };
 }
+
+
+//person 객체를 이용하여 CRUD 작업을 진행하는 함수 정의
+//setObject, getObject, updateObject, deleteObject
+//Arrow function
+
+//1. person 객체에 프로퍼티 추가
+//예) setObject(person, 'address', '강남구')
+export const setObject = (obj, key, value) => {
+    obj[key] = value;
+}
+
+//2. 객체의 프로퍼티 값 반환
+export const getObject = (obj, key) => {
+    return obj[key];
+}
+// console.log(getObject(person, 'name'));
+
+// 3. 객체의 프로퍼티 값을 수정
+export const updateObject = (obj, key, newValue) => {
+    obj[key] = newValue;
+}
+
+// 4. 객체의 프로퍼티 삭제
+export const deleteObject = (obj, key) => {
+    delete obj[key];
+}
