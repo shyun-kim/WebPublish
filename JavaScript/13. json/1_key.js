63c7325cbc998d72292b8e0f7b6034cc73b02840
shyun <exriik@gmail.com>
2025-09-04 14:43:34 +0900

    250904

--- JavaScript/13. json/1_key.js
+++ JavaScript/13. json/1_key.js
@@ -1,3 +1,3 @@
-//kobis key = 1387ed83604df30a0c5d9dfdea0cba00
+//kobis key = 2c0579b0529bc5042a8b0c5b3149b18b
 
 // http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=1387ed83604df30a0c5d9dfdea0cba00&targetDt=20120101
