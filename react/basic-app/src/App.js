2ed8dd6d478a8bf963569216a8d01c3eb4c2ce90
moonsg79 <msk3242@naver.com>
2025-09-08 12:44:09 +0900

    250908

---
+++ react/basic-app/src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="App">Hello~ React App</h1>
+      <h1 className="App">Hello~ React App</h1>
+      <h1 className="App">Hello~ React App</h1>
+      <h1 className="App">Hello~ React App</h1>
+      <h1 className="App">Hello~ React App</h1>
+      <h1 className="App">Hello~ React App</h1>
+      <h1 className="App">Hello~ React App</h1>
+      <h1 className="App">Hello~ React App</h1>
+      <h1 className="App">Hello~ React App</h1>
+    </div>
+  );
+}
+
+export default App;
