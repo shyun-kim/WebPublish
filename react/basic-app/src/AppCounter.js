19aa46138c1445d92e98496fe6ee322b1f50511f
shyun <exriik@gmail.com>
2025-09-09 16:08:16 +0900

    250908

--- react/basic-app/src/AppCounter.js
+++ react/basic-app/src/AppCounter.js
@@ -1,7 +1,28 @@
 import { Counter } from "./components/Counter.jsx";
+import { useState } from "react";
+import './App.css'
+
+// 자식 > 부모(누적합) > 자식(결과 전송)
 
 export default function App() {
+    const [total, setTotal] = useState(0);
+
+    const click = (type) => { //자식 컴포넌트의 클릭 이벤트 결과 가져오기
+        if(type === "+") {
+            setTotal(total+1)
+        } else if(type === "-") {
+            setTotal(total-1)
+        } else {
+            setTotal(0);
+        };
+
+    }
+        
     return (
-        <Counter />
+        <>
+            <h1>Counter Test</h1>
+            <Counter click = {click} total={total}/>
+            <Counter click = {click} total={total}/>
+        </>
     );
 }
--- react/basic-app/src/components/Counter.jsx
+++ react/basic-app/src/components/Counter.jsx
@@ -1,32 +1,56 @@
 import { useState } from "react";
 
-export function Counter() {
-    const [total, setTotal] = useState(0);
+export function Counter({click, total, init}) {
+    // console.log(init, typeof init);
+    
+    const [number, setNumber] = useState(0);
+
     const handleClickIncrement = () => {
-        (total < 10) ? setTotal(total+1) : setTotal(total);
+        if (number < 10) {
+            setNumber(number+1)
+            click("+");
+        } else {
+            setNumber(number);
+        }
     }
     const handleClickDecrement = () => {
-        // if(total <= 0) {
-        //     setTotal(0)
-        // } else {
-        //     setTotal(total-1);
-        // }
-
-        (total > 0) ? setTotal(total-1) : setTotal(0);
+        if(number > 0) {
+            setNumber(number-1);
+            click("-")
+        } else {
+            setNumber(0)
+        }
     }
     const handleClickInit = () => {
-        setTotal(0);
+        click(0);
+        setNumber(0);
+        
     }
     return (
-        <div style={{width: "200px", padding: "10px 20px", border: "1px solid blue"}}>
-            <span style={{display: "block", fontSize:20}}>{total}</span>
-            <button type="button"
-                    onClick={handleClickDecrement}>-(감소)</button>
-            <button type="button"
-                    onClick={handleClickIncrement}>+(증가)</button>
-            <button type="button"
-                    onClick={handleClickInit}>초기화</button>
+        <div className ="counter-container">
+            <div>
+            <span className="number">{number}</span>
+            <span>/</span>
+            <span className="total-number">{total}</span>
+            </div>
+            <div>
+                <button type="button"
+                        onClick={handleClickDecrement}>-(감소)</button>
+                <button type="button"
+                        onClick={handleClickIncrement}>+(증가)</button>
+                <button type="button"
+                        onClick={handleClickInit}>초기화</button>
+            </div>
         </div>
+        // <div style={{width: "200px", padding: "10px 20px", border: "1px solid blue"}}>
+        //     <span style={{display: "block", fontSize:20}}>{total}</span>
+        //     <button type="button"
+        //             onClick={handleClickDecrement}>-(감소)</button>
+        //     <button type="button"
+        //             onClick={handleClickIncrement}>+(증가)</button>
+        //     <button type="button"
+        //             onClick={handleClickInit}>초기화</button>
+        // </div>
     );
 
 }
