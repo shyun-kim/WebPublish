b0ee5e923e57688d225bf173a58a4de86c611ef9
shyun <exriik@gmail.com>
2025-09-09 14:49:21 +0900

    250908

--- react/basic-app/src/components/Counter.jsx
+++ react/basic-app/src/components/Counter.jsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
 
 export function Counter() {
-    let total = 0;
-    const handleClick = () => {
-        total = total+1;
-        console.log(total);
+    const [total, setTotal] = useState(0);
+    const handleClickIncrement = () => {
+        (total < 10) ? setTotal(total+1) : setTotal(total);
+    }
+    const handleClickDecrement = () => {
+        // if(total <= 0) {
+        //     setTotal(0)
+        // } else {
+        //     setTotal(total-1);
+        // }
+
+        (total > 0) ? setTotal(total-1) : setTotal(0);
+    }
+    const handleClickInit = () => {
+        setTotal(0);
     }
     return (
         <div style={{width: "200px", padding: "10px 20px", border: "1px solid blue"}}>
             <span style={{display: "block", fontSize:20}}>{total}</span>
             <button type="button"
-                    onClick={handleClick}>클릭</button>
+                    onClick={handleClickDecrement}>-(감소)</button>
+            <button type="button"
+                    onClick={handleClickIncrement}>+(증가)</button>
+            <button type="button"
+                    onClick={handleClickInit}>초기화</button>
         </div>
     );
 
