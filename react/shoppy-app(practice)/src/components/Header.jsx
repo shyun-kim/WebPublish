bb2c8f3e4a6c2d0edbc2173c65a57a6d7bdbf8c0
shyun <exriik@gmail.com>
2025-09-26 15:41:39 +0900

    250926

---
+++ react/shoppy-app(practice)/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import { FiShoppingBag } from "react-icons/fi";
+import { GiShoppingCart } from "react-icons/gi";
+
+export function Header({cartCount}) {
+
+    return (
+        <div className="header-outer">
+            <div>
+
+            </div>
+        </div>
+    )
+}
