1bf8f35566c1787d1be83fd02765d99c765522f0
shyun <exriik@gmail.com>
2025-10-10 18:18:01 +0900

    251010

--- react/shoppy-app-hook/src/App.js
+++ react/shoppy-app-hook/src/App.js
@@ -9,6 +9,7 @@ import { ProductDetail } from './pages/ProductDetail.jsx';
 import { Cart } from './pages/Cart.jsx';
 import { CheckoutInfo } from './pages/CheckoutInfo.jsx';
 import { Support } from './pages/Support.jsx';
+import { AuthProvider } from './context/AuthContext.js';
 
 import { cartItemsCheck, updateCartItemsQty } from './utils/cart.js';
 
@@ -16,50 +17,43 @@ import './styles/cgvSignup.css';
 import './styles/cgv.css';
 import './styles/commons.css';
 import './styles/shoppy.css';
+import { ProductProvider } from './context/ProductContext.js';
+import { CartProvider } from './context/CartContext.js';
+import { ProtectedPageRoute } from './pages/ProtectedPageRoute.js';
 
 
 
 export default function App() {
-  const [cartCount, setCartCount] = useState(0); //1. 장바구니 수량 관리
-  const [cartItems, setCartItems] = useState([]); //2. 장바구니 아이템 관리
-
-  const addCart = (cartItem) => { // <-- ProductDetail 쇼핑백 추가 이벤트 처리
-    setCartItems(cartItemsCheck(cartItems, cartItem));
-    setCartCount(cartCount+1);
-  }
-
-  const updateCart = (cid, type) => {
-    if(type === undefined) {
-      const findItem = cartItems.fin(item => item.cid === cid);
-      setCartCount(cartCount - findItem.qty);
-
-      setCartItems((cartItems) => {
-        return cartItems.filter(item => !(item.cid === cid));
-      });
-
-    } else {
-      setCartItems(updateCartItemsQty(cartItems, cid, type));
-      type === "+" ? setCartCount(cartCount+1)
-        : cartCount > 1 ?setCartCount(cartCount-1): setCartCount(cartCount);
-    }
-  }
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout cartCount={cartCount} />}>
-          <Route index element={<Home/>} />
-          <Route path="/all" element={<Products/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/signup" element={<Signup/>} />
-          <Route path="/cart" element={<Cart  items={cartItems}
-                                              updateCart={updateCart}/>} />
-          <Route path="/products/:pid" element={<ProductDetail addCart={addCart} />}/>
-          <Route path="/checkout" element={<CheckoutInfo />} />
-          <Route path="/support" element={<Support />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  )
-
+    <AuthProvider>
+      <ProductProvider>
+        <CartProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route index element={<Home/>} />
+                <Route path="/all" element={<Products/>} />
+                <Route path="/login" element={<Login/>} />
+                <Route path="/signup" element={<Signup/>} />
+                <Route path="/cart" element={ 
+                  <ProtectedPageRoute>
+                    <Cart />
+                  </ProtectedPageRoute>} />
+                <Route path="/products/:pid" element={<ProductDetail />}/>
+                <Route path="/checkout"
+                  element={
+                  <ProtectedPageRoute>
+                    <CheckoutInfo />
+                  </ProtectedPageRoute> } />
+                <Route path="/support" element={
+                  <ProtectedPageRoute>
+                    <Support />
+                  </ProtectedPageRoute>} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </CartProvider>
+      </ProductProvider>
+    </AuthProvider>    
+  );
 }
